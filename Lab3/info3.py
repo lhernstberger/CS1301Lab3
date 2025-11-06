ab9d529f4196c36ddf45f905100be7f32d6fbf1d
lhernstberger <lhernstberger@gmail.com>
2025-11-06 00:11:21 -0500

    Add files via upload

---
+++ Lab3/info3.py
@@ -0,0 +1,86 @@
+
+#This File will contain the information to be displayed in your portfolio
+
+#CHANGE BELOW
+profile_picture = "Images/unknown.jpeg"
+about_me = "Lucas is my name, and Chemistry is my Game."
+popcorn_image_url = "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e2/Cinema_popcorn_bucket.jpg/1920px-Cinema_popcorn_bucket.jpg"
+un_image_url = "https://upload.wikimedia.org/wikipedia/commons/2/2f/Flag_of_the_United_Nations.svg"
+hitchcock_url = "https://upload.wikimedia.org/wikipedia/commons/9/94/Hitchcock%2C_Alfred_02.jpg"
+
+#CHANGE BELOW (OPTIONAL)
+letterboxd_image_url = "https://a.ltrbxd.com/logos/letterboxd-decal-dots-pos-rgb-500px.png"
+github_image_url = "https://cdn-icons-png.flaticon.com/256/25/25231.png"
+email_image_url = "https://logowik.com/content/uploads/images/513_email.jpg"
+
+#CHANGE BELOW
+my_letterboxd_url = "https://boxd.it/3PhL9"
+my_github_url = "https://github.com/lhernstberger"
+my_email_address = "lernstberger3@gatech.edu"
+
+
+education_data ={
+    'Degree': 'Bachelor of Science in Chemistry',
+    'Institution': 'Georgia Institute of Technology',
+    'Location': 'Atlanta, GA',
+    'Graduation Date': '2029 (anticipated)',
+    'GPA': '4.0'
+}
+education_data_two ={
+    'Degree': 'Associates of Arts in Interdisciplinary Studies with a concentration in Chemistry and Mathmatics',
+    'Institution': 'LaGrange College',
+    'Location': 'LaGrange, GA',
+    'Graduation Date': '2025',
+    'GPA': '4.0'
+}
+course_data = {
+    "chem":["CHEM 1101+L", "CHEM 3201+L", "CHEM 3202+L", "CHEM 2216+L"], 
+    "names":["General Chemistry II + Lab", "Organic Chemistry I + Lab", "Organic Chemistry II + Lab", "Quantitative Analysis + Lab"], 
+    "semester_taken":["Dual Enrollment", "Dual Enrollment", "Dual Enrollment", "1st"],
+    "skills":["Acids and Bases", "Naming Organic Compounds", "Understanding Organic Reaction Mechanisms", "Titrations"],
+    }
+experience_data = {
+    "Work Study Student at LC Library (2023-2025)": (["- Worked on organizing the film, music, and fiction sections",
+                                                                          "- Answered dozens of phone calls", "- Was on payroll longer than every current permanent member of staff"],"Images/LCLibrary.webp"),
+    #"Cleaner at Krusty Krab":(["- Made the floor almost reflective",
+    #                                                       "- Increased customer visits by three folds by making restaurant clean"],"Images/cleaner.jpg"),
+    #"Jelly Fish Catcher":(["- Caught 100+ Jellyfish with the help of my colleague Patrick"],"Images/jelly.jpg")
+
+}
+
+projects_data = {
+    "Web Dev Lab 1": "A website page that generates movie recommendations based on a series of questions.",
+    "Web Dev Lab 2": "A website page that displays graphical representations of movie data.",
+    "Web Dev Lab 3": "A website page that utilizes an AI chatbot to display the IMDb API."
+}
+
+programming_data = {
+    "Python": 30,
+#    "Java": 70,
+ #   "C": 40,
+}
+
+#CHANGE BELOW (OPTIONAL)
+programming_icons = {
+    "Python": "🐍",
+  #  "Java": "☕",
+  #  "C": "🔍",
+}
+spoken_icons = {"French": "🇫🇷",
+    "English": "🇬🇧",
+    "Spanish":"🇪🇸"
+}
+
+#CHANGE BELOW
+spoken_data = {
+    "English": "Fluent",
+    "Spanish": "Learning",
+}
+leadership_data = {
+    "LHS Chess Club Founder": (["- and president too!"],"Images/ChessSet.jpg"),
+
+}
+activity_data={
+    "Boy Scouts": ["- Made benches for playgrounds.", 
+            "- Led weekly meetings"]
+}
---
+++ Lab3/pages/1Lucas' Portfolio.py
@@ -0,0 +1,110 @@
+import streamlit as st
+import info3
+import pandas as pd
+#about me
+def about_me_section():
+    st.header("About Me")
+    st.image(info3.profile_picture, width = 200)
+    st.write(info3.about_me)
+    st.write("Go Jackets!")
+    st.write("---")
+about_me_section()
+
+
+
+#sidebar links
+def links_section():
+    st.sidebar.header("Links")
+    st.sidebar.text("See my cinema taste on Letterboxd")
+    letterboxd_link=f'<a href="{info3.my_letterboxd_url}"><img src="{info3.letterboxd_image_url}" alt="letterboxd" width ="75" height ="75"></a>'
+    st.sidebar.markdown(letterboxd_link, unsafe_allow_html=True)
+    st.sidebar.text("Checkout my work")
+    github_link = f'<a href="{info3.my_github_url}"><img src="{info3.github_image_url}" alt ="Github" width="65" height="65"></a>'
+    st.sidebar.markdown(github_link, unsafe_allow_html=True)
+    st.sidebar.text("Or email me!")
+    email_html = f'<a href = "mailto:{info3.my_email_address}"><img src="{info3.email_image_url}" alt ="Email" width ="75" height ="75"></a>'
+    st.sidebar.markdown(email_html, unsafe_allow_html=True)
+links_section()
+
+
+# education
+def education_section(education_data, education_data_two, course_data):
+    st.header("Education")
+    st.subheader(f"**{education_data['Institution']}**")
+    st.write(f"**Degree:** {education_data['Degree']}")
+    st.write(f"**Graduation Date:** {education_data['Graduation Date']}")
+    st.write(f"**GPA::** {education_data['GPA']}")
+    st.subheader(f"**{education_data_two['Institution']}**")
+    st.write(f"**Degree:** {education_data_two['Degree']}")
+    st.write(f"**Graduation Date:** {education_data_two['Graduation Date']}")
+    st.write(f"**GPA::** {education_data_two['GPA']}")
+    st.subheader("**Relevant Coursework:**")
+    coursework = pd.DataFrame(course_data)
+    st.dataframe(coursework, column_config={
+        "chem":"Course Code",
+        "names": "Course Names",
+        "semester_taken": "Semester Taken",
+        "skills": "What I Learned"},
+        hide_index=True,
+        )
+    
+    st.write("---")
+education_section(info3.education_data, info3.education_data_two, info3.course_data)
+
+#Professional Experience
+
+def experience_section(experience_data):
+    st.header("Professional Experience")
+    for job_title, (job_description, image) in experience_data.items():
+        expander=st.expander(f"{job_title}")
+        expander.image(image, width=250)
+        for bullet in job_description:
+            expander.write(bullet)
+    st.write("---")
+experience_section(info3.experience_data)
+
+#Projects
+
+def project_section(projects_data):
+    st.header("Projects")
+    for project_name, project_description in projects_data.items():
+        expander=st.expander(f"{project_name}")
+        expander.write(project_description)
+    st.write("---")
+project_section(info3.projects_data)
+
+#skills
+
+def skills_section(programming_data, spoken_data):
+    st.header("Skills")
+    st.subheader("Programming Languages")
+    for skill, percentage in programming_data.items():
+        st.write(f"{skill} {info3.programming_icons.get(skill, )}")
+        st.progress(percentage)
+    st.subheader("Spoken Languages")
+    for spoken, proficiency in spoken_data.items():
+        st.write(f"{spoken} {info3.spoken_icons.get(spoken, )}: {proficiency}")
+    st.write("---")
+skills_section(info3.programming_data, info3.spoken_data)
+
+#activities
+
+def activities_section(leadership_data, activity_data):
+    st.header("Activities")
+    tab1,tab2 = st.tabs(["Leadership","Community"])
+    with tab1:
+        st.subheader("Leadership")
+        for title, (details, image) in leadership_data.items():
+            expander=st.expander(f"{title}")
+            expander.image(image, width=250)
+            for bullet in details:
+                expander.write(bullet)
+    with tab2:
+        st.subheader("Community Service")
+        for title, details in activity_data.items():
+            expander=st.expander(f"{title}")
+            for bullet in details:
+                expander.write(bullet)
+    st.write("---")
+activities_section(info3.leadership_data, info3.activity_data)
+        
