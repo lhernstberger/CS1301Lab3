f803f69f16faac5f2f0e948b6d8c2472db48af19
BruhThatGuy <pmdubagunta@gmail.com>
2025-11-12 21:37:58 -0500

    Create info.py

---
+++ Lab3/info.py
@@ -0,0 +1,87 @@
+
+#This File will contain the information to be displayed in your portfolio
+
+#CHANGE BELOW
+profile_picture = "Images/IMG_1872.jpeg"
+about_me = "I'm Pranav Dubagunta. I'm a first year Industrial Engineering Student from Arizona. I love watching Basketball, and playing at the CRC.  "
+
+
+#CHANGE BELOW (OPTIONAL)
+linkedin_image_url = "https://content.linkedin.com/content/dam/me/business/en-us/amp/brand-site/v2/bg/LI-Bug.svg.original.svg"
+github_image_url = "https://cdn-icons-png.flaticon.com/256/25/25231.png"
+email_image_url = "https://logowik.com/content/uploads/images/513_email.jpg"
+
+#CHANGE BELOW
+my_linkedin_url = "www.linkedin.com/in/pranav-dubagunta-0686b1288"
+my_github_url = "https://github.com/BruhThatGuy/Marketplace"
+my_email_address = "pmdubagunta@gmail.com"
+
+
+education_data ={
+    'Degree': 'Bachelor of Science in Industrial Engineering',
+    'Institution': 'Georgia Institute of Technology',
+    'Location': 'Atlanta, GA',
+    'Graduation Date': '2029',
+    'GPA': 'N/A'
+}
+course_data = {
+    "code":["CS 1301", "English 1102", "MATH 1554", "Physics 2211"],
+    "names":["Intro to CS", "Villians & Outsiders", "Linear Algebra", "Classic Mechanics"],
+    "semester_taken":["1st", "1st", "1st", "1st"],
+    "skills":["Python is not my favorite snake anymore", "Lost my interest in Movies", "Basically row reduction", "If I ever have to hear about Torque... Thankfully I'm an Imaginary Engineer"],
+    }
+experience_data = {
+    "Disney": ([
+        "Pls Disney give me an Internshi*p",
+        "- Increased Company Expenses by 200 dollars (my Internship stipend)",
+        "- Made Walt Disney Proud"
+    ], "Images/disney.jpg"),
+
+    "Eloqiuently collaborated on Sophisticated tasks": ([
+        "I need that FAANG JOB",
+        "- Improved my word-salad",
+        "Made the World a better Place!"
+    ], "Images/FAANG-small.jpg"),
+
+    "Consulting intern at KPMG": ([
+        "Increased KPMG's AURA"
+    ], "Images/KPMG.jpg")
+}
+
+projects_data = {
+    "Aggrova": "Developed an innovative marketplace platform enabling single-location restaurants and grocery stores to achieve volume-based cost discounts through multi-sourcing opportunities and order aggregation. Clustering algorithms for efficient supplier distribution and density-based aggregation, connecting 15 stores and 5 suppliers during beta-testing phase.",
+    "NBA-Trivia Game" : "Developed a trivia game discovering niche facts about the NBA"
+}
+
+programming_data = {
+    "Python": 15,
+    "Java": 5,
+    "C": 0.005,
+}
+
+#CHANGE BELOW (OPTIONAL)
+programming_icons = {
+    "Python": "🐍",
+    "Java": "☕",
+    "C": "🔍",
+}
+spoken_icons = {"Telugu": "🇮🇳",
+    "English": "🇬🇧",
+    "Spanish":"🇪🇸"
+}
+
+#CHANGE BELOW
+spoken_data = {
+    "English": "Fluent",
+    "Telugu": "Fluent",
+    "Spanish": "Me no hablo Espanol",
+}
+leadership_data = {
+    "Founder/Entreprenuer": (["I'm a risk Taker, an adventurer, and spend all of my time scrollin Linkedin. On a real note( if you are reading this), pls connect with me. I need the 500 connections. Yes, I'm Down Bad"],"Images/linkedin.jpg"),
+
+}
+activity_data={
+    "Math Club": ["Volunteered as a tutor",
+            " AMC Prep (Never Qualified :( )"]
+}
+
